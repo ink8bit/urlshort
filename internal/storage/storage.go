76290be3b87916f337c659f1853b83be02f0f9f9
ink8bit <ink8bit@users.noreply.github.com>
2024-01-21 21:00:45 +0300

    Add support for postgresql database

--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -5,4 +5,13 @@ import "errors"
 var (
 	ErrOrigURLNotFound  = errors.New("original url not found")
 	ErrShortURLNotFound = errors.New("short url not found")
+	ErrDBConnection     = errors.New("cannot connect to the databse")
 )
+
+type Storager interface {
+	SaveURL(origURL string) (string, error)
+	FindURL(shortURL string) (string, error)
+	FindShortURL(origURL string) (string, error)
+	Cleanup() error
+	Ping() error
+}
